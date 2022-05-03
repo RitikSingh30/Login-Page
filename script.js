2b4b18fe59f5afdf734f0c0c652ad735a3b2b4e2
ritik singh <ritikraj.rr87@gmail.com>
2022-05-03 20:34:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// this is not working mai sare x , y , z variable function ke andar declar kiya tb jake ye kaam kiya 
+// let x = document.getElementById('login');
+// let y = document.getElementById('register');
+// let z = document.getElementById('btn');
+
+function register(){
+    let x = document.getElementById('login');
+    let y = document.getElementById('register');
+    let z = document.getElementById('btn');
+    x.style.left = '-400px';
+    y.style.left = '50px';
+    z.style.left = '110px';
+}
+
+function login(){
+    let x = document.getElementById('login');
+    let y = document.getElementById('register');
+    let z = document.getElementById('btn');
+    x.style.left = '50px';
+    y.style.left = '450px';
+    z.style.left = '0px';
+}
